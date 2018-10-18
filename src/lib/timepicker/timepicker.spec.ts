dfb0a47d21305aaa9433c8c39e653664bf6a80e3
Andrey Chulkov <achulkov@ptsecurity.com>
2018-10-18 14:56:32 +0300

    fix: Incorrect async test functions expectation

--- src/lib/timepicker/timepicker.spec.ts
+++ src/lib/timepicker/timepicker.spec.ts
@@ -92,36 +92,40 @@ describe('McTimepicker', () => {
         });
 
         it('Should accept simple time set', () => {
-            fixture.whenStable().then(() => {
-                fixture.detectChanges();
-                expect(inputElementDebug.nativeElement.classList.contains('ng-valid')).toBe(true);
-            });
+            return fixture.whenStable()
+                .then(() => {
+                    fixture.detectChanges();
+                    expect(inputElementDebug.nativeElement.classList.contains('ng-valid')).toBe(true);
+                });
         });
 
         it('Should validate if time in min-max range', () => {
-            fixture.whenStable().then(() => {
-                testComponent.minTime = '10:00:00';
-                testComponent.maxTime = '23:00:00';
-                fixture.detectChanges();
-                expect(inputElementDebug.nativeElement.getAttribute('min-time')).toBe('10:00:00');
-                expect(inputElementDebug.nativeElement.getAttribute('max-time')).toBe('23:00:00');
-            });
+            return fixture.whenStable()
+                .then(() => {
+                    testComponent.minTime = '10:00:00';
+                    testComponent.maxTime = '23:00:00';
+                    fixture.detectChanges();
+                    expect(inputElementDebug.nativeElement.getAttribute('min-time')).toBe('10:00:00');
+                    expect(inputElementDebug.nativeElement.getAttribute('max-time')).toBe('23:00:00');
+                });
         });
 
         it('Should invalidate time lower then min-time', () => {
-            fixture.whenStable().then(() => {
-                testComponent.minTime = '13:59:00';
-                fixture.detectChanges();
-                expect(inputElementDebug.nativeElement.classList.contains('ng-invalid')).toBe(true);
-            });
+            return fixture.whenStable()
+                .then(() => {
+                    testComponent.minTime = '13:59:00';
+                    fixture.detectChanges();
+                    expect(inputElementDebug.nativeElement.classList.contains('ng-invalid')).toBe(true);
+                });
         });
 
         it('Should invalidate time higher then max-time', () => {
-            fixture.whenStable().then(() => {
-                testComponent.maxTime = '11:00:00';
-                fixture.detectChanges();
-                expect(inputElementDebug.nativeElement.classList.contains('ng-invalid')).toBe(true);
-            });
+            return fixture.whenStable()
+                .then(() => {
+                    testComponent.maxTime = '11:00:00';
+                    fixture.detectChanges();
+                    expect(inputElementDebug.nativeElement.classList.contains('ng-invalid')).toBe(true);
+                });
         });
     });
 
@@ -135,20 +139,74 @@ describe('McTimepicker', () => {
             fixture.detectChanges();
         });
 
-        it('Convert input', () => {
-            fixture.whenStable().then(() => {
-                testComponent.timeFormat = 'HH:mm:ss';
-                inputElementDebug.nativeElement.value = '18:08:08';
-                inputElementDebug.triggerEventHandler('input', { target: inputElementDebug.nativeElement });
-                fixture.detectChanges();
+        it('Convert input, format HH:mm:ss', () => {
+            return fixture.whenStable()
+                .then(() => {
+                    testComponent.timeFormat = 'HH:mm:ss';
+                    inputElementDebug.nativeElement.value = '18:08:08';
+                    inputElementDebug.triggerEventHandler('input', { target: inputElementDebug.nativeElement });
+                    fixture.detectChanges();
 
-                return fixture.whenStable();
-            }).then(() => {
-                fixture.detectChanges();
-
-                console.log(testComponent.timeValue); // tslint:disable-line
-                expect(testComponent.timeValue.toString()).toContain('18:08:08');
-            });
+                    return fixture.whenStable();
+                }).then(() => {
+                    fixture.detectChanges();
+                    expect(testComponent.timeValue.toString()).toContain('18:08:08');
+                });
         });
+
+        it('Convert input as direct input (onInput), format HH:mm', () => {
+            return fixture.whenStable()
+                .then(() => {
+                    testComponent.timeFormat = 'HH:mm';
+                    inputElementDebug.nativeElement.value = '18:09';
+                    inputElementDebug.triggerEventHandler(
+                        'input',
+                        { target: inputElementDebug.nativeElement }
+                    );
+                    fixture.detectChanges();
+
+                    return fixture.whenStable();
+                }).then(() => {
+                    fixture.detectChanges();
+                    expect(testComponent.timeValue.toString()).toContain('18:09');
+                });
+        });
+
+        it('Convert input onBlur', () => {
+            return fixture.whenStable()
+                .then(() => {
+                    inputElementDebug.nativeElement.value = '19:08:08';
+                    inputElementDebug.triggerEventHandler(
+                        'blur',
+                        { target: inputElementDebug.nativeElement }
+                    );
+                    fixture.detectChanges();
+
+                    return fixture.whenStable();
+                }).then(() => {
+                    fixture.detectChanges();
+                    expect(testComponent.timeValue.toString()).toContain('19:08:08');
+                });
+        });
+
+    });
+
+    describe('', () => {
+        beforeEach(() => {
+            fixture = TestBed.createComponent(TestApp);
+            testComponent = fixture.debugElement.componentInstance;
+            inputElementDebug = fixture.debugElement.query(By.directive(McTimepicker));
+
+            testComponent.timeValue = new Date('1970-01-01 08:08:08');
+            fixture.detectChanges();
+        });
+
+        it('Increase hours by ArrowUp', () => {
+            // TODO Code test
+            return fixture.whenStable().then(() => {
+                expect(2).toBe(2);
+            });
+
+        })
     });
 });
